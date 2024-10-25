d659a763575e2dbabd1861de5efa82f10b982615
prem-credsoft <premcredsoft@gmail.com>
2024-10-25 19:34:12 +0530

    Update vendor-list.php

--- page-template/vendor-list.php
+++ page-template/vendor-list.php
@@ -54,7 +54,7 @@ get_sidebar('dashboard');
                       </thead>
                       <tbody>
                         <?php
-                        $vendor_items = get_vendor_list_items();
+                        $vendor_items = get_vendor_item();
                         ?>
                         <?php if ($vendor_items): ?>
                             <?php foreach ($vendor_items as $vendor): ?>
