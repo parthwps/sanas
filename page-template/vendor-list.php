c55a0f2929907d8ff237cccbfd162507000fef2d
parthwps <parth@netdevs.net>
2024-10-26 02:25:01 +0530

    Update vendor-list.php

--- page-template/vendor-list.php
+++ page-template/vendor-list.php
@@ -62,9 +62,9 @@ get_sidebar('dashboard');
                             <?php foreach ($vendor_items as $vendor): ?>
                                 <tr>
                                     <td><input type="checkbox"></td>
-                                    <td class="text-single-line"><?php echo esc_html($vendor['category']); ?></td>
-                                    <td class="text-single-line"><?php echo esc_html($vendor['name']); ?></td>
-                                    <td class="text-single-line"><?php echo esc_html($vendor['email']); ?></td>
+                                    <td class="text-single-line" data-toggle="tooltip" data-bs-original-title="<?php echo esc_html($vendor['category']); ?>"><?php echo esc_html($vendor['category']); ?></td>
+                                    <td class="text-single-line" data-toggle="tooltip" data-bs-original-title="<?php echo esc_html($vendor['name']); ?>"><?php echo esc_html($vendor['name']); ?></td>
+                                    <td class="text-single-line" data-toggle="tooltip" data-bs-original-title="<?php echo esc_html($vendor['email']); ?>"><?php echo esc_html($vendor['email']); ?></td>
                                     <td><?php echo esc_html($vendor['phone']); ?></td>
                                     <td class="text-single-line" data-toggle="tooltip" data-bs-original-title="<?php echo esc_html($vendor['notes']); ?>"><?php echo esc_html($vendor['notes']); ?></td>
                                     <td><?php echo esc_html($vendor['social_media_profile']); ?></td>
