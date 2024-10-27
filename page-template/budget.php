8006ce46002afe6fa360ecd0261016f00e38ad4b
prem-credsoft <premcredsoft@gmail.com>
2024-10-27 15:31:09 +0530

    Update budget.php

--- page-template/budget.php
+++ page-template/budget.php
@@ -96,7 +96,11 @@ get_sidebar('dashboard');
                       <?php if ($categories): ?>
                       <?php foreach ($categories as $category): ?>
                         <li><a href="#">
-                          <div class="ttl"><i class="fa-solid fa-a"></i><span class="txt"><?php echo esc_html($category['category_name']); ?></span>
+                          <div class="ttl">
+                            <div class="circle-icon">
+                              <i class="fa-solid fa-<?php echo strtolower(substr($category['category_name'], 0, 1)); ?>"></i>
+                            </div>
+                            <span class="txt"><?php echo esc_html($category['category_name']); ?></span>
                           </div>
                           <div class="count"><span>$<?php echo esc_html($category['cost']); ?></span> <i class="fa fa-trash"></i></div>
                         </a></li>
