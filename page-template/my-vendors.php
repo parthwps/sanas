82c9084f8eb5826c289472160cc2767c2c0e67cb
prem-credsoft <premcredsoft@gmail.com>
2024-10-29 20:24:47 +0530

    Update my-vendors.php

--- page-template/my-vendors.php
+++ page-template/my-vendors.php
@@ -38,7 +38,7 @@ get_sidebar('dashboard');
                 <div class="inner-box3">
                   <div class="table-box upcoming-tasks">
                     <div class="table-responsive m-0">
-                      <table class="table" id="vendor-table">
+                      <table class="vendor-table" id="vendor-table">
                         <thead>
                         <tr class="todo-check-title">
                           <th> <input type="checkbox" name="allCheck" id="all-select-chechbox"> </th>
