6cdae0f2994c6d0a26a6f1a0ebf303d469f6dca7
prem-credsoft <premcredsoft@gmail.com>
2024-10-29 12:40:02 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -430,7 +430,7 @@
         $('#guest-list-Table').DataTable();
         $('#vendor-table').DataTable({
             columnDefs: [
-                { orderable: true, targets: [1, 6] },
+                { orderable: true, targets: [1, 5] },
             ]
         });
         $('#guest-contact-list').DataTable();
