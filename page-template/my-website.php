921dbf922e52b76fe4abbc5da925f2ca8cf35ddd
parthwps <parth@netdevs.net>
2024-11-07 00:13:01 +0530

    Create my-website.php

---
+++ page-template/my-website.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+    Template Name: Budget 
+    * The template for displaying all pages
+    *
+    * This is the template that displays all pages by default.
+    * Please note that this is the WordPress construct of pages
+    * and that other 'pages' on your WordPress site may use a
+    * different template.
+    *
+    * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+    *
+    * @package sanas
+*/
+get_header();
+get_sidebar('dashboard');
+?>
+<h1 class="text-center">Coming Soon</h1>
+<?php
+get_footer('dashboard');
+?>
