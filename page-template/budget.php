54fd18e9a06e120c0cf332c88a905c906d3ee0df
parthwps <parth@netdevs.net>
2024-11-08 20:33:53 +0530

    Update budget.php

--- page-template/budget.php
+++ page-template/budget.php
@@ -141,7 +141,7 @@ get_sidebar('dashboard');
                             ?>
                             
                             <li <?php echo (empty($_GET['category']) && $index === 0) || (isset($_GET['category']) && $_GET['category'] == $category_id) ? ' class="active"' : ''; ?>>
-                                <a href="javascript:void(0)" class="budget-category-item">
+                                <a href="/budget/?category=<?php echo esc_attr($category_id); ?>#budget-expense-box"> <!--  class="budget-category-item" -->
                                     <div class="ttl" data-id="<?php echo esc_attr($category['id']); ?>">
                                         <i class="fa-solid fa-<?php echo !empty($category['icon_class']) ? esc_attr($category['icon_class']) : strtolower(substr($category['category_name'], 0, 1)); ?>"></i>
                                         <span class="txt"><?php echo esc_html($category['category_name']); ?></span>
