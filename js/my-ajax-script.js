4e7b104a7cbe502aa687ae13a782f62d4d119ce0
prem-credsoft <premcredsoft@gmail.com>
2024-11-08 14:03:05 +0530

    Update my-ajax-script.js

--- js/my-ajax-script.js
+++ js/my-ajax-script.js
@@ -60,108 +60,105 @@ if (window.location.pathname === '/budget/') {
                 }
             }
         });
-    jQuery('.budget-category-item').on('click', function() {
-        var categoryId = jQuery(this).data('id');
-        jQuery('#category-id-input').val(categoryId);
+jQuery('.budget-category-item').on('click', function() {
+    var categoryId = jQuery(this).data('id');
+    jQuery('#category-id-input').val(categoryId);
 
-        jQuery('#category_cost_section li').removeClass('active');
-        jQuery(this).parent().addClass('active');
-        var categoryText = jQuery(this).find('span.txt').text();
-        jQuery('.category_name_box').html(categoryText);
-        var categoryIcon = jQuery(this).find('.ttl i').prop('outerHTML');
-        jQuery('#budget-expense-box .icon-box').html(categoryIcon);
-    
-        $.ajax({
-            method: 'POST',
-            url: ajax_object.ajax_url,
-            data: {
-                category_id: categoryId,
-                action: 'get_budget_expense_by_category'
-            },
-            success: function(response) {
-                jQuery('html, body').animate({
-                    scrollTop: jQuery('#budget-expense-box').offset().top
-                }, 300);
-                window.history.pushState(null, '', '?category=' + categoryId);
-                if (response.success) {
-                    var expenses = response.data.expenses;
-                    if (expenses.length === 0) {
-                        jQuery('#budget-expense tbody').html('<tr><td colspan="8">No expenses to display.</td></tr>');
-                        return;
-                    }
-            
-                    var total_estimated = 0;
-                    var total_actual = 0;
-                    var total_paid = 0;
-                    var total_due = 0;
-                    var rows = '';
-                    expenses.forEach(function(expense) {
-                        total_estimated += parseFloat(expense.estimated_cost);
-                        console.log('estimated', total_estimated);
-                        total_actual += parseFloat(expense.actual_cost);
-                        console.log('actual', total_actual);
-                        total_paid += parseFloat(expense.paid);
-                        console.log('paid', total_paid);
-                        total_due += parseFloat(expense.actual_cost) - parseFloat(expense.paid);
-                        console.log('due', total_due);
-            
-                        rows += '<tr>' +
-                                    '<td class="expense text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.expense) + '">' + escapeHtml(expense.expense) + '</td>' +
-                                    '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.vendor_name) + '">' + escapeHtml(expense.vendor_name) + '</td>' +
-                                    '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + (expense.vendor_contact ? '+' + escapeHtml(expense.vendor_contact) : '') + '">' + (expense.vendor_contact ? '+' + escapeHtml(expense.vendor_contact) : '') + '</td>' +
-                                    '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.estimated_cost) + '">$' + escapeHtml(expense.estimated_cost) + '</td>' +
-                                    '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.actual_cost) + '">$' + escapeHtml(expense.actual_cost) + '</td>' +
-                                    '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.paid) + '">$' + escapeHtml(expense.paid) + '</td>' +
-                                    '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + (parseFloat(expense.actual_cost) - parseFloat(expense.paid)).toFixed(2) + '">$' + (parseFloat(expense.actual_cost) - parseFloat(expense.paid)).toFixed(2) + '</td>' +
-                                    '<td class="actions">' +
-                                        '<a href="#" class="edit theme-btn" data-id="' + escapeHtml(expense.id) + '" data-bs-toggle="modal" data-bs-target="#edit-expense-popup">' +
-                                            '<i class="fa-solid fa-pen"></i>' +
-                                        '</a>' +
-                                        '<a href="#" class="delete theme-btn" data-id="' + escapeHtml(expense.id) + '">' +
-                                            '<i class="fa-regular fa-trash-can"></i>' +
-                                        '</a>' +
-                                    '</td>' +
-                                '</tr>';
-                    });
-            
-                    // Add total row
-                    rows += '<tr class="expense-total-row">' +
-                                '<td class="text-single-line">Total</td>' +
-                                '<td>&nbsp;</td>' +
-                                '<td>&nbsp;</td>' +
-                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + total_estimated.toFixed(2) + '">$' + total_estimated.toFixed(2) + '</td>' +
-                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + total_actual.toFixed(2) + '">$' + total_actual.toFixed(2) + '</td>' +
-                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + total_paid.toFixed(2) + '">$' + total_paid.toFixed(2) + '</td>' +
-                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + total_due.toFixed(2) + '>$' + total_due.toFixed(2) + '</td>' +
-                                '<td class="actions">&nbsp;</td>' +
-                            '</tr>';
-            
-                    // Update the table body with the new rows
-                    jQuery('#budget-expense tbody').html(rows);
-                    jQuery('.category_estimated').text(total_estimated.toFixed(2));
-                    jQuery('.category_actual').text(total_actual.toFixed(2));
-                } else {
+    jQuery('#category_cost_section li').removeClass('active');
+    jQuery(this).parent().addClass('active');
+    var categoryText = jQuery(this).find('span.txt').text();
+    jQuery('.category_name_box').html(categoryText);
+    var categoryIcon = jQuery(this).find('.ttl i').prop('outerHTML');
+    jQuery('#budget-expense-box .icon-box').html(categoryIcon);
+
+    $.ajax({
+        method: 'POST',
+        url: ajax_object.ajax_url,
+        data: {
+            category_id: categoryId,
+            action: 'get_budget_expense_by_category'
+        },
+        success: function(response) {
+            jQuery('html, body').animate({
+                scrollTop: jQuery('#budget-expense-box').offset().top
+            }, 300);
+            window.history.pushState(null, '', '?category=' + categoryId);
+            if (response.success) {
+                var expenses = response.data.expenses;
+                if (expenses.length === 0) {
                     jQuery('#budget-expense tbody').html('<tr><td colspan="8">No expenses to display.</td></tr>');
+                    return;
                 }
-            },
-            
-            error: function() {
-                alert('Error loading expenses.');
+
+                var total_estimated = 0;
+                var total_actual = 0;
+                var total_paid = 0;
+                var total_due = 0;
+                var rows = '';
+                expenses.forEach(function(expense) {
+                    total_estimated += parseFloat(expense.estimated_cost);
+                    total_actual += parseFloat(expense.actual_cost);
+                    total_paid += parseFloat(expense.paid);
+                    total_due += parseFloat(expense.actual_cost) - parseFloat(expense.paid);
+
+                    rows += '<tr>' +
+                                '<td class="expense text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.expense) + '">' + escapeHtml(expense.expense) + '</td>' +
+                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.vendor_name) + '">' + escapeHtml(expense.vendor_name) + '</td>' +
+                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + (expense.vendor_contact ? '+' + escapeHtml(expense.vendor_contact) : '') + '">' + (expense.vendor_contact ? '+' + escapeHtml(expense.vendor_contact) : '') + '</td>' +
+                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.estimated_cost) + '">$' + escapeHtml(expense.estimated_cost) + '</td>' +
+                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.actual_cost) + '">$' + escapeHtml(expense.actual_cost) + '</td>' +
+                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + escapeHtml(expense.paid) + '">$' + escapeHtml(expense.paid) + '</td>' +
+                                '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + (parseFloat(expense.actual_cost) - parseFloat(expense.paid)).toFixed(2) + '">$' + (parseFloat(expense.actual_cost) - parseFloat(expense.paid)).toFixed(2) + '</td>' +
+                                '<td class="actions">' +
+                                    '<a href="#" class="edit theme-btn" data-id="' + escapeHtml(expense.id) + '" data-bs-toggle="modal" data-bs-target="#edit-expense-popup">' +
+                                        '<i class="fa-solid fa-pen"></i>' +
+                                    '</a>' +
+                                    '<a href="#" class="delete theme-btn" data-id="' + escapeHtml(expense.id) + '">' +
+                                        '<i class="fa-regular fa-trash-can"></i>' +
+                                    '</a>' +
+                                '</td>' +
+                            '</tr>';
+                });
+
+                // Add total row
+                rows += '<tr class="expense-total-row">' +
+                            '<td class="text-single-line">Total</td>' +
+                            '<td>&nbsp;</td>' +
+                            '<td>&nbsp;</td>' +
+                            '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + total_estimated.toFixed(2) + '">$' + total_estimated.toFixed(2) + '</td>' +
+                            '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + total_actual.toFixed(2) + '">$' + total_actual.toFixed(2) + '</td>' +
+                            '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + total_paid.toFixed(2) + '">$' + total_paid.toFixed(2) + '</td>' +
+                            '<td class="text-single-line" data-toggle="tooltip" data-bs-original-title="' + total_due.toFixed(2) + '">$' + total_due.toFixed(2) + '</td>' +
+                            '<td class="actions">&nbsp;</td>' +
+                        '</tr>';
+
+                // Update the table body with the new rows
+                jQuery('#budget-expense tbody').html(rows);
+                jQuery('.category_estimated').text(total_estimated.toFixed(2));
+                jQuery('.category_actual').text(total_actual.toFixed(2));
+                jQuery('.category_due').text(total_due.toFixed(2)); // Ensure total due is displayed
+            } else {
+                jQuery('#budget-expense tbody').html('<tr><td colspan="8">No expenses to display.</td></tr>');
             }
-        });
+        },
+        
+        error: function() {
+            alert('Error loading expenses.');
+        }
     });
-    // Escape HTML for safety
-    function escapeHtml(text) {
-        if (text == null) return ''; // Return empty string for null or undefined
-        var map = {
-            '&': '&amp;',
-            '<': '&lt;',
-            '>': '&gt;',
-            '"': '&quot;',
-            "'": '&#39;'
-        };
-        return text.replace(/[&<>"']/g, function(m) { return map[m]; });
-    }
+});
+// Escape HTML for safety
+function escapeHtml(text) {
+    if (text == null) return ''; // Return empty string for null or undefined
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;'
+    };
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
 
     // Edit Expense Item
     jQuery(document).on('click', '.edit-expense', function(e) {
