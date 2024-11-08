8c3a54bf87aaea4fa8ff55f59392b6497d481ea4
prem-credsoft <premcredsoft@gmail.com>
2024-11-08 18:54:12 +0530

    Update budget.php

--- page-template/budget.php
+++ page-template/budget.php
@@ -449,8 +449,8 @@ get_sidebar('dashboard');
                   </div>
                   <div class="form-group col-lg-12 col-sm-12">
                     <div class="links-box">
-                      <input type="hidden" name="category_id" id="category-id-input" value="<?php echo isset($first_category) ? esc_attr($first_category) : ''; ?>">
-                      <input type="hidden" name="id" id="edit-expense-id">
+                      <input type="text" name="category_id" id="category-id-input" value="<?php echo isset($first_category) ? esc_attr($first_category) : ''; ?>">
+                      <input type="text" name="id" id="edit-expense-id">
                       <button type="submit" class="dashbord-btn">Save</button>
                     </div>
                   </div>
