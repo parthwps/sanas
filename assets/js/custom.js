fd8d63500f8fd5ba4381b7a027aeb25c8039d9d7
parthwps <parth@netdevs.net>
2024-11-12 01:46:45 +0530

    test

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,6 +17,12 @@
         }
     });
 
+    jQuery(document).ready(function() {
+        const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85a1.007 1.007 0 0 0-.115-.098zm-5.442 1.106a5.5 5.5 0 1 1 0-7.778 5.5 5.5 0 0 1 0 7.778z"/>
+        </svg>`;
+        jQuery(".dt-search label").html(svgIcon);
+      });
 
 // jQuery(document).on('click', function (e) {
 //         if (!jQuery(e.target).closest('.form-boxed').length) {
