00fc9dfa4b00f55aeb845eb7b40d7c29aca3e347
parthwps <parth@netdevs.net>
2024-11-12 07:41:30 +0530

    Update my-ajax-script.js

--- js/my-ajax-script.js
+++ js/my-ajax-script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
       
         tooltipTriggerEl.addEventListener('shown.bs.tooltip', () => {
           const tooltipElement = document.querySelector('.tooltip'); // Get the tooltip element
-          tooltipElement.style.top = `${parseInt(tooltipElement.style.top) - 10}px`; // Adjust top by -5px
+          tooltipElement.style.top = `${parseInt(tooltipElement.style.top) - 50}px`; // Adjust top by -5px
           tooltipElement.style.left = `${parseInt(tooltipElement.style.left) - 20}px`; // Adjust left by +10px
         });
       
