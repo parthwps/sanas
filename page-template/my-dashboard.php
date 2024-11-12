449c3b54421b9a5b495a5178fad462b584de0b32
parthwps <parth@netdevs.net>
2024-11-12 08:28:10 +0530

    Update my-dashboard.php

--- page-template/my-dashboard.php
+++ page-template/my-dashboard.php
@@ -693,7 +693,7 @@ $totals = $wpdb->get_row(
         var myLineChart = new Chart(ctx, {
             type: 'pie',
             data: {
-                labels: ["Spring", "Summer"],
+                labels: ["Pending", "Sent"],
                 datasets: [{
                     data: [20, 50],
                     backgroundColor: ["rgba(255, 0, 0, 0.5)", "rgba(100, 255, 0, 0.5)"]
